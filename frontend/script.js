f9ea1162f973cfd9bf229e2807f6ff61cb32e493
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2022-10-26 02:26:13 -0300

    gerar bilhete request

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,22 @@
+let bilhete;
+const baseUrl = "http://localhost:5000/api"
+
+async function gerarBilhete(){
+    console.log("gerando bilhete")
+
+    //fazendo o get 
+    const response = await fetch(baseUrl+"/bilhetes", {
+        method: 'GET',
+        
+    })
+    .then((response) =>response.json())
+    .then((json) =>{
+        console.log(json)
+        bilhete = json
+        console.log(bilhete.codigo);
+    })
+
+}
+
+gerarBilhete()
 console.log("aaaaa");
