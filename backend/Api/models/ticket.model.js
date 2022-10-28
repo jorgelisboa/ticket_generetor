16142569fa519fad2897c5961c2d20dcb4d26dcc
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2022-10-28 20:59:29 -0300

    conexao oracle cloud.

--- backend/Api/models/ticket.model.js
+++ backend/Api/models/ticket.model.js
@@ -4,12 +4,14 @@ oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
 
 oracledb.autoCommit = true;
 
+oracledb.initOracleClient({ libDir: 'C:\\instantclient_21_7' });
+
 const recuperarBilhetes = async () => {
   let connection;
   connection = await oracledb.getConnection({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    connectString: process.env.DB_CONNECTIONSTRING || "CEATUDB02:1521/xe", 
+    user: 'admin',
+    password: 'TicketGen2022',
+    connectString: `(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1522)(host=adb.sa-saopaulo-1.oraclecloud.com))(connect_data=(service_name=g6f8bc328ff6f14_ticketgenerate_high.adb.oraclecloud.com))(security=(ssl_server_cert_dn="CN=adb.sa-saopaulo-1.oraclecloud.com, OU=Oracle ADB SAOPAULO, O=Oracle Corporation, L=Redwood City, ST=California, C=US")))`, 
   });
 
   let randomCode = new Date().getTime();
