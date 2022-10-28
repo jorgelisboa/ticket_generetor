8b8ce52e39bed6964d65e8ab70d224ad105b01b7
JOÃO PEDRO DE OLIVEIRA CANDIDO <joao.poc2@puccampinas.edu.br>
2022-10-28 18:21:37 -0300

    CORS global config no router backend.

--- backend/Api/router.js
+++ backend/Api/router.js
@@ -2,21 +2,10 @@ const express = require("express");
 const ticketController = require("./controllers/ticket.controller");
 const cors = require("cors");
 
-const corsOpts = {
-    origin: '*',
-  
-    methods: [
-      'GET',
-      'POST',
-    ],
-  
-    allowedHeaders: [
-      'Content-Type',
-    ],
-  };
-
 const router = express.Router();
 
-router.get("/api/bilhetes", cors(corsOpts), ticketController.recuperarBilhetes);
+router.use(cors())
+
+router.get("/api/bilhetes", ticketController.recuperarBilhetes);
 
 module.exports = router;
