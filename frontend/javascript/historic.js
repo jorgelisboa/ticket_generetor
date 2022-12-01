11cca07fe3da48c1b192a0a8676e7b594f5f7326
jonathan lucas <113566274+jonathanluca@users.noreply.github.com>
2022-12-01 01:18:01 -0300

    funções do historico exboçadas em js

--- frontend/javascript/historic.js
+++ frontend/javascript/historic.js
@@ -1,7 +1,38 @@
 function getHistoric(numeroBilhete){
-
+   // BaseURL
+   let historicoBilhete
+   const baseUrl = 'http://localhost:5000/api'
+   axios.get(`${baseUrl}/recargas/${numeroBilhete}`)
+   .then((response) => {
+     if (response.data.status == 200)
+       //Ir pra página do cartão gerado (levar valores do cartão)
+       console.log('Bilhete utilizado.', response.data)
+    historicoBilhete = response.data
+   })
+   .catch((error) => {
+     console.log('err',error.request.response);
+   });
+    return historicoBilhete
 }
 
 function getNumeroBilhete(){
     return document.getElementById("historic_input").value
 }
+
+function criarLinhasTabelas(listaDoHistorico){
+    let tabela = document.getElementById("tabela_historico")
+    // acahar minha tabela no html usando o id
+    let linhas
+    for (let index = 0; index < listaDoHistorico.length; index++) {
+      linhas = `<tr>
+      <td>${listaDoHistorico[index].horario}</td>
+      <td>${listaDoHistorico[index].status}</td>
+      <td>${listaDoHistorico[index].tipoRecargas}</td>
+      </tr>`  
+    }
+    // criar o html de uma linha da tebela.
+
+    tabela.innerHTML = linhas
+    // colocar no doc html essa mudanças
+}
+
